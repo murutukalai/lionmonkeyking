9347e5047d671b85ae9ffe9ffc44f4f1dc3a1e83
murutukalai <158081785+murutukalai@users.noreply.github.com>
2024-06-07 08:07:51 +0530

    Update Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -1,1 +1,4 @@
 https://www.blackbox.ai/share/28611b3f-a074-4678-ac7f-eb2db88d5ca8
+
+
+https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=794279055-s6lgamd96qgq5hfiqooj57uk29rf9cav.apps.googleusercontent.com&state=1aIWIxKTpsB2gvoOYp1MCw&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fauth&scope=openid+email+profile&nonce=y1gLyFxXpqEQaVecJhm2zQ
